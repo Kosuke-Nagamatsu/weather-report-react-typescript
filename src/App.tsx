28f87789fef96445619c5fd1af21cbfbf71e55b9
kosuke_nagamatsu <hateruma39@gmail.com>
2022-08-24 01:00:06 +0900

    add: loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import type { FC } from 'react';
 import { WeatherItem } from './components/WeatherItem';
-import { useFetchWeather } from './hooks/useFetchWeathers';
+import { useFetchWeather } from './hooks/useFetchWeather';
 
 export const App: FC = () => {
-  const { weather, isError } = useFetchWeather();
+  const { weather, isError, isLoading } = useFetchWeather();
   const { name, main, temp, icon } = weather;
+
   return (
     <>
-      {isError ? (
-        <p style={{ color: 'red' }}>エラーが発生しました</p>
+      {isError && <p style={{ color: 'red' }}>エラーが発生しました</p>}
+
+      {isLoading ? (
+        <p style={{ color: 'blue' }}>天気予報を取得中</p>
       ) : (
         <WeatherItem name={name} main={main} temp={temp} icon={icon} />
       )}
